74fce74e14dc69e4bea55416d7f2705901a2a2d7
Daniel R <danramteke>
2021-06-18 18:58:22 -0400

    first draft

---
+++ Formula/amalgamation.rb
@@ -0,0 +1,51 @@
+class Amalgamation < Formula
+  desc "Command-line interface for SQLite with full amalgamtion"
+  homepage "https://sqlite.org/"
+  url "https://www.sqlite.org/2021/sqlite-autoconf-3360000.tar.gz"
+  version "3.36.0"
+  sha3 "fdc699685a20284cb72efe3e3ddfac58e94d8ffd5b229a8235d49265aa776678"
+
+  depends_on "readline"
+  uses_from_macos "zlib"
+
+  def install
+    ENV.append "CPPFLAGS", "-DSQLITE_ENABLE_COLUMN_METADATA=1"
+    # Default value of MAX_VARIABLE_NUMBER is 999 which is too low for many
+    # applications. Set to 250000 (Same value used in Debian and Ubuntu).
+    ENV.append "CPPFLAGS", "-DSQLITE_MAX_VARIABLE_NUMBER=250000"
+    ENV.append "CPPFLAGS", "-DSQLITE_ENABLE_RTREE=1"
+    ENV.append "CPPFLAGS", "-DSQLITE_ENABLE_FTS3=1 -DSQLITE_ENABLE_FTS3_PARENTHESIS=1"
+    ENV.append "CPPFLAGS", "-DSQLITE_ENABLE_JSON1=1"
+    ENV.append "CPPFLAGS", "-DSQLITE_ENABLE_MATH_FUNCTIONS"
+    ENV.append "CPPFLAGS", "-DSQLITE_ENABLE_FTS4"
+    ENV.append "CPPFLAGS", "-DSQLITE_ENABLE_FTS5"
+    ENV.append "CPPFLAGS", "-DSQLITE_ENABLE_MATH_FUNCTIONS"
+    ENV.append "CPPFLAGS", "-DSQLITE_ENABLE_GEOPOL"
+    ENV.append "CPPFLAGS", "-DSQLITE_THREADSAFE"
+
+    args = %W[
+      --prefix=#{prefix}
+      --disable-dependency-tracking
+      --enable-dynamic-extensions
+      --enable-readline
+      --disable-editline
+      --enable-session
+    ]
+
+    system "./configure", *args
+    system "make", "install"
+  end
+
+  test do
+    path = testpath/"school.sql"
+    path.write <<~EOS
+      create table students (name text, age integer);
+      insert into students (name, age) values ('Bob', 14);
+      insert into students (name, age) values ('Sue', 12);
+      insert into students (name, age) values ('Tim', 13);
+      select name from students order by age asc;
+    EOS
+
+    names = shell_output("#{bin}/sqlite3 < #{path}").strip.split("\n")
+    assert_equal %w[Sue Tim Bob], names
+  end
